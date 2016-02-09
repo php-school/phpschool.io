1b43f39cff3dba8e02e06163c8852156fc3a3465
Shane Osbourne <shane.osbourne8@gmail.com>
2016-02-09 19:28:11 +0000

    chore: cleanup 1. Use crossbow layouts 2. Enable Pretty urls 3. Use header/footer partials

--- tasks/crossbow.js
+++ tasks/crossbow.js
@@ -9,7 +9,9 @@ function crossbowBuild (obs, opts, ctx) {
     ctx.vfs.src(input)
         .pipe(crossbow.stream({
             config: {
+                defaultLayout: "default.hbs",
                 base: opts.base,
+                prettyUrls: true,
                 errorHandler: function (err, compiler) {
                     err.crossbowMessage = obs.compile(compiler.getErrorString(err)[0])
                     obs.onError(err);
