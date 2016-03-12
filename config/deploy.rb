775a2ff48a7b8e2e8c02cf0a93e3f1706e5e15a1
Aydin Hassan <aydin@hotmail.co.uk>
2016-03-12 20:29:57 +0100

    Clear cache on deploy, update branch, lower logging level

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,22 @@ lock '3.4.0'
 
 set :composer_install_flags, '--no-dev --no-interaction --quiet --optimize-autoloader'
 set :application, 'phpschool'
-set :repo_url, 'https://github.com/php-school/php-school.github.io'
-set :branch, 'fw-me-up'
+set :repo_url, 'git@github.com:php-school/phpschool.io.git'
+set :branch, 'master'
 set :deploy_to, '/var/www/html'
 set :pty, true
+set :log_level, :info
+
+namespace :deploy do
+
+  task :clear_cache do
+    on roles(:web) do |host|
+      within release_path do
+          execute "php #{release_path}/bin/app clear-cache"
+      end
+    end
+  end
+
+
+  before 'deploy:updated', 'deploy:clear_cache'
+end
