a7567d703c52d0b8e5c0dc09a848f063d8929ecb
Aydin Hassan <aydin@hotmail.co.uk>
2016-03-12 19:50:18 +0100

    Update Website URL and put in config

--- app/config.php
+++ app/config.php
@@ -48,11 +48,12 @@ $config = [
         ],
 
         'links' => [
-            'github'        => 'https://github.com/php-school/learn-you-php',
-            'twitter'       => 'https://twitter.com/PHPSchoolTeam',
-            'slack'         => 'https://phpschool.herokuapp.com',
-            'discussions'   => 'https://github.com/php-school/discussions',
-            'workshop'      => 'https://github.com/php-school/php-workshop',
+            'github'         => 'https://github.com/php-school/learn-you-php',
+            'twitter'        => 'https://twitter.com/PHPSchoolTeam',
+            'slack'          => 'https://phpschool.herokuapp.com',
+            'discussions'    => 'https://github.com/php-school/discussions',
+            'workshop'       => 'https://github.com/php-school/php-workshop',
+            'github-website' => 'https://github.com/php-school/phpschool.io',
         ],
 
         'cacheDir'          => __DIR__ . '/../cache',
