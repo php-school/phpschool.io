51b57f982ce969221954320828642c93e59d3ca7
Aydin Hassan <aydin@hotmail.co.uk>
2016-08-03 23:49:02 +0200

    Fix doctrine CLI

--- cli-config.php
+++ cli-config.php
@@ -1,15 +1,11 @@
 <?php
 
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use DI\ContainerBuilder;
+use PhpSchool\Website\ContainerFactory;
 
 require __DIR__ . '/vendor/autoload.php';
 
 ini_set('display_errors', 1);
 
-$containerBuilder = new ContainerBuilder;
-$containerBuilder->addDefinitions(__DIR__ . '/vendor/php-di/slim-bridge/src/config.php');
-$containerBuilder->addDefinitions(__DIR__ . '/app/config.php');
-
-$container = $containerBuilder->build();
+$container = (new ContainerFactory)();
 return $container->get(ConsoleRunner::class);
