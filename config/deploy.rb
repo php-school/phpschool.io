32f3af13d802842ceb8f9ea571f0a209ac47d3e7
Aydin Hassan <aydin@hotmail.co.uk>
2016-08-04 21:16:35 +0200

    link db

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,29 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :composer_install_flags, '--no-dev --no-interaction --quiet --optimize-autoloader'
+set :composer_install_flags, '--no-dev --no-interaction --quiet --optimize-autoloader --ignore-platform-reqs'
 set :application, 'phpschool'
 set :repo_url, 'git@github.com:php-school/phpschool.io.git'
 set :branch, 'master'
 set :deploy_to, '/var/www/html'
 set :pty, true
 set :log_level, :debug
+set :keep_releases, 2
 
+set :linked_dirs, %w{.docker/mysql}
 set :linked_files, %w{public/workshops.json}
 namespace :deploy do
+
+  task :setup_container do
+    on roles(:web) do |host|
+      within release_path do
+        execute('docker-compose', 'stop', ';true')
+        execute('docker', 'rm', '-f', '`docker ps -aq`', ';true')
+        execute('docker-compose', 'build')
+        execute('docker-compose', '-f', 'docker-compose.yml', '-f', 'docker-compose-prod.yml', 'up', '-d')
+      end
+    end
+  end
 
   task :clear_cache do
     on roles(:web) do |host|
@@ -28,6 +41,8 @@ namespace :deploy do
     end
   end
 
-  before 'deploy:finished', 'deploy:clear_cache'
-  after 'deploy:clear_cache', 'deploy:build_api_docs'
+  #before 'deploy:finished', 'deploy:clear_cache'
+  #after 'deploy:clear_cache', 'deploy:build_api_docs'
+
+  after "deploy:finished", "deploy:setup_container"
 end
