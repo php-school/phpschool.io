21dac45e7d88dc5800dbb99abf4b3ffa7b905442
Aydin Hassan <aydin@hotmail.co.uk>
2022-11-03 14:24:13 +0100

    Controller for display exercise problem

---
+++ src/Cloud/Action/ShowExerciseProblem.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace PhpSchool\Website\Cloud\Action;
+
+use PhpSchool\Website\Cloud\CloudWorkshopRepository;
+use PhpSchool\Website\Cloud\ProblemFileConverter;
+use PhpSchool\Website\PhpRenderer;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class ShowExerciseProblem
+{
+    private CloudWorkshopRepository $installedWorkshops;
+    private ProblemFileConverter $problemFileConverter;
+
+    public function __construct(
+        CloudWorkshopRepository $installedWorkshops,
+        ProblemFileConverter $problemFileConverter
+    ) {
+        $this->installedWorkshops = $installedWorkshops;
+        $this->problemFileConverter = $problemFileConverter;
+    }
+
+    public function __invoke(
+        Request $request,
+        Response $response,
+        PhpRenderer $renderer,
+        string $workshop,
+        string $exercise
+    ): Response {
+
+        $workshop = $this->installedWorkshops->findByCode($workshop);
+        $exercise = $workshop->findExerciseByName($exercise);
+
+        return $renderer->render($response, 'layouts/layout.phtml', [
+            'pageTitle'       => 'PHP School Cloud',
+            'pageDescription' => 'PHP School Cloud',
+            'content'         => $renderer->fetch('cloud-exercise-problem.phtml', [
+                'workshop' => $workshop,
+                'exercise' => $exercise,
+                'problem' => $this->problemFileConverter->htmlFromExercise($exercise)
+            ])
+        ]);
+    }
+}
---
+++ src/Cloud/ProblemFileConverter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PhpSchool\Website\Cloud;
+
+use League\CommonMark\CommonMarkConverter;
+use PhpSchool\PhpWorkshop\Exception\ProblemFileDoesNotExistException;
+use PhpSchool\PhpWorkshop\Exercise\ExerciseInterface;
+
+class ProblemFileConverter
+{
+    private CommonMarkConverter $commonMarkConverter;
+
+    public function __construct(CommonMarkConverter $commonMarkConverter)
+    {
+        $this->commonMarkConverter = $commonMarkConverter;
+    }
+
+    public function htmlFromExercise(ExerciseInterface $exercise): string
+    {
+        $problemFile = $exercise->getProblem();
+        if (!is_readable($problemFile)) {
+            throw ProblemFileDoesNotExistException::fromFile($problemFile);
+        }
+
+        return $this->commonMarkConverter->convertToHtml(file_get_contents($problemFile));
+    }
+
+    public function markdownFromExercise(ExerciseInterface $exercise): string
+    {
+        $problemFile = $exercise->getProblem();
+        if (!is_readable($problemFile)) {
+            throw ProblemFileDoesNotExistException::fromFile($problemFile);
+        }
+
+        return file_get_contents($problemFile);
+    }
+}
