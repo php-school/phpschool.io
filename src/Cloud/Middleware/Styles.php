dcbf4c56a6db4b5ead5d7aa730d9c79271652134
Aydin Hassan <aydin@hotmail.co.uk>
2022-11-12 14:39:08 +0100

    Add development/prod vite builds based on env

--- src/Cloud/Middleware/Styles.php
+++ src/Cloud/Middleware/Styles.php
@@ -9,22 +9,93 @@ use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 class Styles
 {
+    private const VITE_HOST = 'http://localhost:5133';
+    /** @var array<string, array>  */
+    private array $manifest;
+
     private PhpRenderer $renderer;
 
     public function __construct(PhpRenderer $renderer)
     {
         $this->renderer = $renderer;
+
+        $content = file_get_contents(__DIR__ . '/../../../public/dist/manifest.json');
+        $this->manifest = json_decode($content, true);
     }
 
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
         $this->renderer
             ->removeCss('code-blocks')
+            ->removeJs('jquery')
             ->removeJs('main-js')
-            ->removeJs('highlight-js')
-            ->appendLocalCss('tailwindcss', __DIR__ . '/../../../public/css/cloud.min.css')
-            ->addJs('vue', 'https://unpkg.com/vue@3/dist/vue.global.js');
+            ->removeJs('highlight-js');
+
+        $this->js('main.js');
+        $this->jsPreloadImports('main.js');
+        $this->css('main.js');
 
         return $handler->handle($request);
+    }
+
+    private function isDevMode(): bool
+    {
+        return isset($_ENV['DEV_MODE']) && $_ENV['DEV_MODE'] === 'true';
+    }
+
+    private function js(string $entry): void
+    {
+        $url = $this->isDevMode()
+            ? self::VITE_HOST . '/' . $entry
+            : $this->assetUrl($entry);
+
+        if (!$url) {
+            return;
+        }
+
+        $this->renderer->addJs('cloud', $url, ['type' => 'module', 'crossorigin']);
+    }
+
+    private function jsPreloadImports(string $entry): void
+    {
+        if ($this->isDevMode()) {
+            return;
+        }
+
+        foreach ($this->importsUrls($entry) as $i => $url) {
+            $this->renderer->addPreload($i, $url);
+        }
+    }
+
+    private function importsUrls(string $entry): array
+    {
+        return array_map(function (array $import) {
+            return '/dist/' . $this->manifest[$import]['file'];
+        }, $this->manifest[$entry]['imports'] ?? []);
+    }
+
+    private function css(string $entry): void
+    {
+        if ($this->isDevMode()) {
+            return;
+        }
+
+        foreach ($this->cssUrls($entry) as $i => $url) {
+            $this->renderer->appendRemoteCss($i, $url);
+        }
+    }
+
+    private function cssUrls(string $entry): array
+    {
+        return array_map(function (string $file) {
+            return '/dist/' . $file;
+        }, $this->manifest[$entry]['css'] ?? []);
+    }
+
+    private function assetUrl(string $entry): string
+    {
+        return isset($this->manifest[$entry])
+            ? '/dist/' . $this->manifest[$entry]['file']
+            : '';
     }
 }
