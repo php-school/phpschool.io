d365d814b56397a783840ab79a9f41e625e338f7
Aydin Hassan <aydin@hotmail.co.uk>
2022-11-15 10:55:48 +0100

    Build components

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,30 @@
 import 'vite/modulepreload-polyfill'
 import './styles'
 import { createApp } from 'vue'
-
+import AceEditor from './components/AceEditor.vue'
+import FileTree from './components/FileTree.vue'
+import TreeItem from './components/TreeItem.vue'
+import Tabs from './components/Tabs.vue'
+import Tab from './components/Tab.vue'
+import Modal from './components/Modal.vue'
+import StudentProgress from './components/StudentProgress.vue'
+import WorkshopExerciseSelectionList from './components/WorkshopExerciseSelectionList.vue'
+import PassNotification from "./components/PassNotification.vue";
+import ExerciseVerify from "./components/ExerciseVerify.vue";
+import ExerciseEditor from "./components/ExerciseEditor.vue";
 
 const components = {
+    AceEditor,
+    FileTree,
+    TreeItem,
+    Tabs,
+    Tab,
+    Modal,
+    StudentProgress,
+    WorkshopExerciseSelectionList,
+    PassNotification,
+    ExerciseVerify,
+    ExerciseEditor
 }
 
 const app = createApp({
