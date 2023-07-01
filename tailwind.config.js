09444fbaecd6bc15ed8121aa2c9f63200c60bf02
Rory Martin <rorymartin3d@gmail.com>
2023-07-01 13:09:33 +0100

    Added images to correct location. Started content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  //important: "#page-top",
-  content: ["./templates/**/*.{phtml,js}", "./public/js/cloud.js", "./src/PhpWorkshop/**/*.php", "./assets/**/*.js", "./assets/components/**/*.vue"],
-  theme: {
-    extend: {
-      fontFamily: {
-        'open-sans': ['"Open Sans"'],
-      },
+    //important: "#page-top",
+    content: ['./templates/**/*.{phtml,js}', './public/js/cloud.js', './src/PhpWorkshop/**/*.php', './assets/**/*.js', './assets/components/**/*.vue'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'open-sans': ['"Open Sans"'],
+                'work-sans': ['Work Sans, sans-serif'],
+            },
+            boxShadow: {
+                'brand-shadow': '4px 4px 0px rgba(68, 39, 52, 0.50)',
+            },
+        },
     },
-  },
-  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
-  variants: {
-    scrollbar: ["rounded"],
-  },
+    plugins: [require('tailwind-scrollbar')({ nocompatible: true })],
+    variants: {
+        scrollbar: ['rounded'],
+    },
 };
