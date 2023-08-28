7923717bcff6354a1dcfa1df9d3d9096734ecf73
Aydin Hassan <aydin@hotmail.co.uk>
2023-08-28 20:58:24 +0200

    Extra button component

---
+++ assets/components/Home/Button.vue
@@ -0,0 +1,11 @@
+<script setup>
+ const props = defineProps({
+     'href' : String
+ })
+</script>
+
+<template>
+    <a :href="props.href" class="text-white font-bold text-base  px-10 py-2 uppercase my-4 mr-2 rounded-full bg-pink-600 hover:bg-pink-500 shadow-brand-shadow">
+        <slot />
+    </a>
+</template>
