b30412581753228e4215858993dbb5d05ddcba08
Aydin Hassan <aydin@hotmail.co.uk>
2023-10-19 08:40:24 +0200

    Remove unused important selector

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    //important: "#page-top",
     content: ['./templates/**/*.{phtml,js}', './public/js/cloud.js', './src/PhpWorkshop/**/*.php', './assets/**/*.js', './assets/components/**/*.vue'],
     theme: {
         extend: {
