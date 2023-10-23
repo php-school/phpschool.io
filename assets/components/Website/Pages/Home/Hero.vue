d5cf8ca76a8f36db95270f6d5a80dc3ed6f6753e
Rory Martin <rorymartin3d@gmail.com>
2023-10-23 20:02:21 +0100

    Hero section re-designed

--- assets/components/Website/Pages/Home/Hero.vue
+++ assets/components/Website/Pages/Home/Hero.vue
@@ -1,20 +1,43 @@
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
 import Button from "../../Button.vue";
+import Logo from "../../Logo.vue";
+import codeAnimation from "../../codeAnimation.vue";
+
+
 </script>
 
 <template>
-    <div class="max-w-[2048px] mx-auto  my-8  sm:my-12 md:my-14 lg:my-20">
-        <div class="w-full flex items-center justify-center flex-col  text-center pt-20 pb-10 gap-4 ">
-            <p class="text-white text text-2xl font-sans font-normal">A revolutionary new way to learn PHP</p>
-            <h1 class="text-white text-5xl md:text-6xl font-mono uppercase font-bold tracking-wider">Open source learning
-                for php
-            </h1>
-            <p class="text-white text text-2xl font-sans font-light">Bring your imagination to life in an open learning
-                eco-system</p>
-            <Button href="/cloud">GET STARTED</Button>
+    <div class="bg-center bg-cover bg-[url('/img/cloud/phpCity.jpg')] -mt-28 z-10">
+        <div class="max-w-[2048px] mx-auto w-full h-screen flex justify-evenly items-center text-center pt-28 ">
+            <div class="glow-effect text-white w-[400px] max-h-80 overflow-hidden px-4">
+                <codeAnimation class="text-left " />
+            </div>
+            <div>
+                <figure class="glow-effect">
+                    <Logo class="text-white h-64 w-64 " />
+                </figure>
+                <div class="mt-20">
+                    <a href=" /cloud"
+                        class="text-white uppercase items-center px-10 py-2 rounded-full bg-none font-thin border-2 transition-all duration-300 ease-in-out hover:bg-white hover:text-gray-800 hover:no-underline">
+                        get started
+                    </a>
+                </div>
+            </div>
+            <div class="glow-effect text-white w-[400px] max-h-80 overflow-hidden px-4">
+                <codeAnimation class="text-left" />
+            </div>
         </div>
-        <figure class="w-full flex items-center justify-center flex-col  text-center">
-            <img src="../../../../img/cloud/editor-placeholder.png" alt=" placeholder">
-        </figure>
     </div>
 </template>
+
+<style>
+.glow-effect {
+    filter: drop-shadow(0 0 10px rgba(255, 255, 255, 0.8));
+    transition: filter 0.5s ease-in-out;
+}
+
+.glow-effect:hover {
+    filter: drop-shadow(0 0 20px rgba(255, 255, 255, 1));
+}
+</style>
