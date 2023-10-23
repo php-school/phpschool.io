44a48bd9e1a8063e78aa98921003ab6f2a45de47
Aydin Hassan <aydin@hotmail.co.uk>
2023-10-23 22:00:52 +0200

    Add docs components

--- assets/cloud.js
+++ assets/cloud.js
@@ -23,6 +23,28 @@ import Home from "./components/Website/Pages/Home.vue";
 import SiteNav from "./components/Website/SiteNav.vue";
 import SiteFooter from "./components/Website/SiteFooter.vue";
 import Events from "./components/Website/Pages/Events.vue";
+import SubmitWorkshop from "./components/Website/Pages/SubmitWorkshop.vue";
+import Post from "./components/Website/Pages/Blog/Post.vue";
+
+import hljs from 'highlight.js/lib/core';
+import php from 'highlight.js/lib/languages/php';
+import shell from 'highlight.js/lib/languages/shell';
+import javascript from 'highlight.js/lib/languages/javascript';
+import markdown from 'highlight.js/lib/languages/markdown';
+import Terminal from "./components/Website/Docs/Terminal.vue";
+import ContentHeader from "./components/Website/Docs/ContentHeader.vue";
+import DocCode from "./components/Website/Docs/Code.vue";
+import Note from "./components/Website/Docs/Note.vue";
+import DocList from "./components/Website/Docs/List.vue";
+import DocListItem from "./components/Website/Docs/ListItem.vue";
+import DocTable from "./components/Website/Docs/Table.vue";
+import ExerciseTypes from "./components/Website/Docs/ExerciseTypes.vue";
+
+hljs.registerLanguage('php', php);
+hljs.registerLanguage('shell', shell);
+hljs.registerLanguage('shell', javascript);
+hljs.registerLanguage('md', markdown);
+
 
 const components = {
     AceEditor,
@@ -38,9 +60,19 @@ const components = {
     StudentDropdown,
     ListWorkshops,
     Home,
+    SubmitWorkshop,
     SiteNav,
     SiteFooter,
-    Events
+    Events,
+    Post,
+    Terminal,
+    ContentHeader,
+    DocCode,
+    Note,
+    DocList,
+    DocListItem,
+    DocTable,
+    ExerciseTypes
 }
 
 const app = createApp({components});
@@ -56,3 +88,5 @@ app.use(VueDiff);
 app.use(VueShepherdPlugin);
 
 app.mount('#app');
+
+hljs.highlightAll();
