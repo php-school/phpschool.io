5bb7acd9e8d9adf384c7bb8dfc9f789d2f294be0
Aydin Hassan <aydin@hotmail.co.uk>
2023-11-06 23:53:06 +0100

    Allow button to render as a or button

--- assets/components/Website/Button.vue
+++ assets/components/Website/Button.vue
@@ -1,11 +1,12 @@
 <script setup>
  const props = defineProps({
-     'href' : String
+     'href' : String,
+     'as': String
  })
 </script>
 
 <template>
-    <a :href="props.href" class="text-white font-bold text-base  px-10 py-2 uppercase my-4 mr-2 rounded-full bg-pink-600 hover:bg-pink-500 shadow-brand-shadow">
+    <component :href="props.href" :is="href ? 'a' : 'button'" class="text-white font-bold text-base  px-10 py-2 uppercase my-4 mr-2 rounded-full bg-pink-600 hover:bg-pink-500 shadow-brand-shadow">
         <slot />
-    </a>
+    </component>
 </template>
