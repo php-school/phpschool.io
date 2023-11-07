e5261cc10cc62cf3f339d5a2b14e7fa32615c3e9
Aydin Hassan <aydin@hotmail.co.uk>
2023-11-07 22:38:46 +0100

    Use page header component

--- assets/components/Website/Pages/Blog/Post.vue
+++ assets/components/Website/Pages/Blog/Post.vue
@@ -1,23 +1,33 @@
 <script setup>
+import Heading from "../Home/Heading.vue";
 
 const props = defineProps({
-    'post': String,
-    'meta': Object
+    post: String,
+    meta: Object
 })
 
 </script>
 
 <template>
-    <div class="font-open-sans flex flex-col mx-auto max-w-3xl text-left text-gray-800 p-8">
-        <div class="text-center mb-3">
-            <h1 class="text-4xl font-bold leading-normal mt-[9px] mb-3 mx-0 font-mono text-[#e91e63]">{{ meta.title }}</h1>
-            <span class="text-xs font-bold hover:text-[#e91e63] hover:underline"><a :href="meta.authorLink">{{ meta.author }}</a></span>
-            <span class="text-xs text-gray-400"> - {{ meta.date }}</span>
-        </div>
+    <section class="bg-gradient-to-b from-gray-900 to-cyan-500 bg-fixed items-stretch">
+        <Heading>
+            <template v-slot:title>
+                What's happening in <br>the PHP School world
+            </template>
+        </Heading>
+    </section>
+    <div class="w-full bg-gray-900">
+        <div class="font-open-sans flex flex-col mx-auto max-w-3xl text-left text-gray-300 p-8">
+            <div class="text-center mb-3">
+                <h1 class="text-4xl font-bold leading-normal mt-[9px] mb-3 mx-0 font-mono text-[#e91e63]">{{ meta.title }}</h1>
+                <span class="text-xs font-bold hover:text-[#e91e63] hover:underline"><a :href="meta.authorLink">{{ meta.author }}</a></span>
+                <span class="text-xs text-gray-400"> - {{ meta.date }}</span>
+            </div>
 
-         <div class="post">
-             <slot />
-         </div>
+             <div class="post">
+                 <slot />
+             </div>
+        </div>
     </div>
 </template>
 
@@ -36,7 +46,7 @@ const props = defineProps({
 }
 
 .post h3 {
-    @apply font-bold text-lg font-mono text-black pt-[13.5px] mb-[13.5px]
+    @apply font-bold text-lg font-mono text-gray-300 pt-[13.5px] mb-[13.5px]
 }
 
 .post ul {
@@ -52,11 +62,11 @@ const props = defineProps({
 
 }
 
-.post pre code.language-shell {
-    @apply p-4 my-5 w-full block bg-gray-800 text-white text-xs leading-relaxed;
+.post pre > code {
+    @apply block p-4 mb-4 text-xs !bg-gray-900 !border !border-gray-600 rounded-md overflow-y-scroll;
 }
 
 .post :not(pre)>code {
-    @apply px-2 py-0.5 mx-1 text-xs bg-gray-200 text-pink-500;
+    @apply px-1 py-0.5 text-[12px] bg-[#2a2c2d] text-pink-500 rounded
 }
 </style>
