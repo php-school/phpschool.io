5f62d55bc4687244f98767fc11d9484c0957cab5
Aydin Hassan <aydin@hotmail.co.uk>
2024-02-13 21:25:28 +0100

    Allow buton to be router link

--- assets/components/Website/PrimaryButton.vue
+++ assets/components/Website/PrimaryButton.vue
@@ -1,12 +1,12 @@
 <script setup>
 const props = defineProps({
   href: String,
-  as: String,
+  to: String,
 });
 </script>
 
 <template>
-  <component :href="props.href" :is="href ? 'a' : 'button'" class="my-4 mr-2 rounded-full bg-pink-600 px-10 py-2 text-base font-bold uppercase text-white shadow-brand-shadow hover:bg-pink-500">
+  <component :href="props.href" :is="to ? 'router-link' : (href ? 'a' : 'button')" :to="to" class="my-4 mr-2 rounded-full bg-pink-600 px-10 py-2 text-base font-bold uppercase text-white shadow-brand-shadow hover:bg-pink-500 cursor-pointer">
     <slot />
   </component>
 </template>
